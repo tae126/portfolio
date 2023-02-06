45100641ce6906027d96f0f54eb0ac9c05c107a4
tae126 <122263047+tae126@users.noreply.github.com>
2023-02-06 16:24:56 +0900

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -12,10 +12,12 @@
 
 $('#header .open').on('click', function() {
     $(this).parent().addClass('on')
+    $(this).parent().find('#nav').stop().fadeIn(400)
 })
 
 $('#header .close').on('click', function() {
     $(this).parents('#header').removeClass('on')
+    $(this).parents('#nav').stop().fadeOut(400)
 })
 
 $('#nav .depth1 > li > a').on('click', function() {
@@ -23,5 +25,10 @@ $('#nav .depth1 > li > a').on('click', function() {
     // $(this).parent().siblings().removeClass('on')
     $(this).parent().find('.depth2').stop().slideToggle()
 })
+
+
+
+
+
 
 
--- js/index.js
+++ js/index.js
@@ -3,7 +3,8 @@ $('.article1 .slide-group').slick({
     autoplaySpeed:4000,
     dots:true,
     speed:500,
-    pauseOnHover:true,
+    pauseOnHover:false,
     prevArrow:'<button class="slick-prev slick-arrow"><span class="arrow"></span><span class="prev">PREV</span></button>',
     nextArrow:'<button class="slick-next slick-arrow"><span class="arrow"></span><span class="next">NEXT</span></button>',
+    
 })
