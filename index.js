5ba1b60eba61c8997264bd1f80d114c46dd13310
tae126 <122263047+tae126@users.noreply.github.com>
2023-02-06 15:24:19 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+$('.article1 .slide-group').slick({
+    autoplay:true,
+    autoplaySpeed:4000,
+    dots:true,
+    speed:500,
+    pauseOnHover:false,
+    prevArrow:'<button class="slick-prev slick-arrow"><span class="arrow"></span><span class="prev">PREV</span></button>',
+    nextArrow:'<button class="slick-next slick-arrow"><span class="arrow"></span><span class="next">NEXT</span></button>',
+})
